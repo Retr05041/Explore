da31ff24eba08c56fb1f97195b877aab1b118442
Retr05041 <parker@cranfield.ca>
2024-07-26 09:27:49 -0700

    Rough outline for playerhandler

--- internal/playerhandler/playerhandler.go
+++ internal/playerhandler/playerhandler.go
@@ -1,25 +1,79 @@
 package playerhandler
 
+import (
+    "database/sql"
+    "log"
+    "errors"
+    "github.com/mattn/go-sqlite3"
+)
+
+var (
+    ErrDuplicate    = errors.New("record already exists")
+    ErrNotExists    = errors.New("row not exists")
+    ErrUpdateFailed = errors.New("update failed")
+    ErrDeleteFailed = errors.New("delete failed")
+)
+
+type Database struct {
+    inst *sql.DB
+}
+
 type Player struct {
     Name string
     Inventory []string
 }
 
-func NewPlayer(name string) (*Player) {
-    tmpPlayer := new(Player)
-    tmpPlayer.Name = name
-    tmpPlayer.Inventory = append(tmpPlayer.Inventory, "TestItem1")
-    tmpPlayer.Inventory = append(tmpPlayer.Inventory, "TestItem2")
-    return tmpPlayer
+func LoadDatabase(filename string) (*Database) {
+    db, err := sql.Open("sqlite3", filename + ".db")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    tmpDB := new(Database) 
+    tmpDB.inst = db
+
+    err = tmpDB.InitTable()
+    if err != nil {
+        log.Fatal(err)
+    }
+    return tmpDB
 }
 
-func (p *Player) IsInInv(item string) (bool) {
-    for _, token := range p.Inventory {
-       if token == item { return true } 
-    } 
-    return false
+func (db *Database) InitTable() error {
+    query := `
+    CREATE TABLE IF NOT EXISTS players(
+        name TEXT NOT NULL UNIQUE
+    );
+    `
+
+    _, err :=  db.inst.Exec(query)
+    return err
 }
 
-func (p *Player) AddToInv(item string) {
-    p.Inventory = append(p.Inventory, item)
+func (db *Database) LoadPlayer(playername string) (*Player,error) {
+    row := db.inst.QueryRow("SELECT * FROM players WHERE name=?", playername)
+
+    var player Player
+    if err := row.Scan(&player.Name); err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return nil, ErrNotExists
+        }
+        return nil, err
+    }
+    return &player, nil
+}
+
+func (db *Database) CreatePlayer(name string) error {
+    _, err := db.inst.Exec("INSERT INTO players (name) VALUES(?)", name)
+    if err != nil {
+        var sqliteErr sqlite3.Error
+        if errors.As(err, &sqliteErr) {
+            if errors.Is(sqliteErr.ExtendedCode, sqlite3.ErrConstraintUnique) {
+                return ErrDuplicate
+            }
+        }
+        return err
+    }
+
+    return nil
 }
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"explore/internal/commander"
-	"explore/internal/tui"
+	//"explore/internal/commander"
+	//"explore/internal/tui"
+    "log"
+    "fmt"
+    "explore/internal/playerhandler"
 )
 
 
 func main() {
     // Menu
     // Get menu option
+    test := playerhandler.LoadDatabase("prologue")
+    test.CreatePlayer("testplayer")
+    currentPlayer, err := test.LoadPlayer("testplayer")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(currentPlayer.Name)
 
-    commander.Init("./maps/prologue.json") // Run tui once commander is setup
+    //commander.Init("./maps/prologue.json") // Run tui once commander is setup
 
-    tui.Start()
+    //tui.Start()
 }
